4b0ff2eda05afaeaa6e815274a2820ae88b4aec6
John Forster <j_forster@hotmail.co.uk>
2018-07-23 16:14:28 +0100

    Updated view tests for single, multiple and no notes

--- spec/note-list-view-spec.js
+++ spec/note-list-view-spec.js
@@ -1,5 +1,16 @@
 (function(exports){
-  function testHtmlListsNotes(){
+  function testHtmlListsSingleNote(){
+    let note1 = new NoteModel('This is a test note.');
+    let noteList = new NoteListModel();
+    noteList.add(note1);
+    let view = new NoteListView(noteList);
+    let expectedHtml = '<li><div>Note 1: This is a test note.</div></li>';
+    if(view.html() !== expectedHtml){
+      throw new Error('HTML not what was expected (single).');
+    }
+  }
+
+  function testHtmlListsMultipleNotes(){
     let note1 = new NoteModel('This is a test note.');
     let note2 = new NoteModel('This is a test note 2');
     let noteList = new NoteListModel();
@@ -8,8 +19,20 @@
     let view = new NoteListView(noteList);
     let expectedHtml = '<li><div>Note 1: This is a test note.</div></li><li><div>Note 2: This is a test note 2</div></li>'
     if(view.html() !== expectedHtml){
-      throw new Error('HTML not what was expected.');
+      throw new Error('HTML not what was expected (multiple).');
     }
   }
-  testHtmlListsNotes();
+
+  function testHtmlDoesntListEmptyNote(){
+    let noteList = new NoteListModel();
+    let view = new NoteListView(noteList);
+    let expectedHtml = '';
+    if(view.html() !== expectedHtml){
+      throw new Error('HTML not what was expected (no notes).');
+    }
+  }
+
+  testHtmlDoesntListEmptyNote();
+  testHtmlListsSingleNote();
+  testHtmlListsMultipleNotes();
 })(this);
