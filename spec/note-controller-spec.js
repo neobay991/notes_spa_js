808be3e3e7b0214eb6dd82164c1548740409a0c2
Jay Khan <jkhan992@gmail.com>
2018-07-26 15:47:16 +0100

    Refactor: updated NoteController and updated unit test

--- spec/note-controller-spec.js
+++ spec/note-controller-spec.js
@@ -1,15 +1,10 @@
 (function(exports){
   function testNoteController(){
     var noteController = new NoteController(noteList = new NoteListModel());
-
-    var note1 = new NoteModel('This is a test note.');
-    noteList.add(note1);
-    var view = new NoteListView(noteList);
-
-
-    expectedHtml = "Note 1: This is a test note."
+    expectedHtml = "<div><li>Note 1: Favourite drink: seltzer</li></div>"
     if(noteController.html() !== expectedHtml){
-      throw new Error('HTML not what was expected (multiple).');
+
+      throw new Error('HTML not what was expected.');
     }
   }
   testNoteController();
--- src/note-controller.js
+++ src/note-controller.js
@@ -1,26 +1,28 @@
 (function(exports){
-
   function NoteController(noteList = new NoteListModel()){
     let note1 = new NoteModel("Favourite drink: seltzer");
     // let noteList = new NoteListModel();
     noteList.add(note1);
     let view = new NoteListView(noteList);
     return view.html();
+    // html();
+
   }
 
   // public method
-  NoteController.prototype.html = function () {
+  NoteController.prototype.html = function(){
     let appDiv = document.getElementById('app');
 
     // update index.html with what is returned from NoteController() function
     appDiv.innerHTML = NoteController();
+    return appDiv.innerHTML;
   }
 
   // invoke the method to return the html
   NoteController.prototype.html();
 
   // make the constructor public for tests
-  exports.NoteController = NoteController(noteList = new NoteListModel());
+  exports.NoteController = NoteController;
 
 })(this);
 
